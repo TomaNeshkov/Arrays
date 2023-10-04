98afd67bc519a49bd6a5c4d140dfb564fd01ea3a
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-04 21:46:50 +0300

    Array exercises

---
+++ 1. Days of The Week/1. Days of The Week/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        int dayNumber = int.Parse(Console.ReadLine());
+
+        if (dayNumber >= 1 && dayNumber <= 7)
+        {
+            string day = days[dayNumber - 1];
+            Console.WriteLine(day);
+        }
+        else
+        {
+            Console.WriteLine("Invalid day!");
+        }
+    }
+}
