cd7f75539ce146335c7bf7ed7fce8dfae7ec1746
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-04 21:56:14 +0300

    Array Exercises

---
+++ 4. Reverse Array of Strings/4. Reverse Array of Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] stringsArray = input.Split(' ');
+        Array.Reverse(stringsArray);
+
+        foreach (string str in stringsArray)
+        {
+            Console.Write(str + " ");
+        }
+    }
+}
