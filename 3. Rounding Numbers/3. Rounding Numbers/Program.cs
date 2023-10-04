04452d78f4349859c8e174f02a2b5730f553b728
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-04 21:54:40 +0300

    Array Exercises (75/100)

---
+++ 3. Rounding Numbers/3. Rounding Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] numberStrings = input.Split(' ');
+
+        foreach (string numberString in numberStrings)
+        {
+            double number = double.Parse(numberString);
+            int roundedNumber = (int)Math.Round(number, MidpointRounding.AwayFromZero);
+            Console.WriteLine($"{number} => {roundedNumber}");
+        }
+    }
+}
