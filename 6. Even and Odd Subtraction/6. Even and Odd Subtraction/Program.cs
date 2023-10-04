7d555b478a3a9dbb0983a0b4f64e6a80d9177aca
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-04 22:02:24 +0300

    Array Exercises

---
+++ 6. Even and Odd Subtraction/6. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] numberStrings = input.Split(' ');
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        foreach (string numberString in numberStrings)
+        {
+            int number = int.Parse(numberString);
+            if (number % 2 == 0)
+            {
+                sumEven += number;
+            }
+            else
+            {
+                sumOdd += number;
+            }
+        }
+
+        int difference = sumEven - sumOdd;
+        Console.WriteLine(difference);
+    }
+}
