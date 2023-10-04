6fa00b0f7af73d3195f99d677255b744e0df89f3
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-04 21:58:31 +0300

    Array Exercises

---
+++ 5. Sum Even Numbers/5. Sum Even Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] numberStrings = input.Split(' ');
+        int sum = 0;
+
+        foreach (string numberString in numberStrings)
+        {
+            int number = int.Parse(numberString);
+
+            if (number % 2 == 0)
+            {
+                sum += number;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
